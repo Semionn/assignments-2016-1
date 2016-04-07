6867abfc2fd3df6e0f58fd2d742f6af63cd59012
Semionn <mrSemionn@gmail.com>
2016-04-07 17:58:38 +0300

    add task 4 (streams)

--- src/main/java/ru/spbau/mit/FirstPartTasks.java
+++ src/main/java/ru/spbau/mit/FirstPartTasks.java
@@ -1,73 +1,112 @@
-package ru.spbau.mit;
+    package ru.spbau.mit;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
+    import java.util.*;
+    import java.util.function.Function;
+    import java.util.stream.Collectors;
+    import java.util.stream.IntStream;
+    import java.util.stream.Stream;
 
-public final class FirstPartTasks {
-    private FirstPartTasks() {}
-    // Список названий альбомов
-    public static List<String> allNames(Stream<Album> albums) {
-        throw new UnsupportedOperationException();
+    public final class FirstPartTasks {
+        private FirstPartTasks() {}
+        // Список названий альбомов
+        public static List<String> allNames(Stream<Album> albums) {
+            return albums
+                    .map(Album::getName)
+                    .collect(Collectors.toList());
+        }
+
+        // Список названий альбомов, отсортированный лексикографически по названию
+        public static List<String> allNamesSorted(Stream<Album> albums) {
+            return albums
+                    .map(Album::getName)
+                    .sorted()
+                    .collect(Collectors.toList());
+        }
+
+        // Список треков, отсортированный лексикографически по названию, включающий все треки альбомов из 'albums'
+        public static List<String> allTracksSorted(Stream<Album> albums) {
+            return albums
+                    .flatMap(album -> album.getTracks().stream())
+                    .map(Track::getName)
+                    .sorted()
+                    .collect(Collectors.toList());
+        }
+
+        // Список альбомов, в которых есть хотя бы один трек с рейтингом более 95, отсортированный по названию
+        public static List<Album> sortedFavorites(Stream<Album> s) {
+            return s
+                    .filter(album -> album.getTracks().stream()
+                            .anyMatch(track -> track.getRating() > 95))
+                    .sorted(Comparator.comparing(Album::getName))
+                    .collect(Collectors.toList());
+        }
+
+        // Сгруппировать альбомы по артистам
+        public static Map<Artist, List<Album>> groupByArtist(Stream<Album> albums) {
+            return albums.collect(Collectors.groupingBy(Album::getArtist));
+        }
+
+        // Сгруппировать альбомы по артистам (в качестве значения вместо объекта 'Artist' использовать его имя)
+        public static Map<Artist, List<String>> groupByArtistMapName(Stream<Album> albums) {
+            return albums.collect(
+                    Collectors.groupingBy(Album::getArtist,
+                    Collectors.mapping(Album::getName, Collectors.toList())));
+        }
+
+        // Число повторяющихся альбомов в потоке
+        public static long countAlbumDuplicates(Stream<Album> albums) {
+            return albums
+                    .collect(Collectors.groupingBy(Album::hashCode))
+                    .entrySet()
+                    .stream()
+                    .mapToInt(entry -> entry.getValue().size() - 1)
+                    .sum();
+        }
+
+        // Альбом в котором максимум рейтинга минимален
+        // (если в альбоме нет ни одного трека, считать, что максимум рейтинга в нем --- 0)
+        public static Optional<Album> minMaxRating(Stream<Album> albums) {
+            return albums
+                    .collect(Collectors.toMap(Function.identity(),
+                            (Album album) -> album.getTracks().stream()
+                                    .mapToInt(Track::getRating)
+                                    .max()
+                                    .orElse(0)))
+                    .entrySet()
+                    .stream()
+                    .min((entry1, entry2) -> entry1.getValue().compareTo(entry2.getValue()))
+                    .map(Map.Entry::getKey);
+        }
+
+        // Список альбомов, отсортированный по убыванию среднего рейтинга его треков (0, если треков нет)
+        public static List<Album> sortByAverageRating(Stream<Album> albums) {
+            return albums
+                    .collect(Collectors.toMap(Function.identity(),
+                            (Album album) -> album.getTracks().stream()
+                                    .mapToInt(Track::getRating)
+                                    .average()
+                                    .orElse(0)))
+                    .entrySet()
+                    .stream()
+                    .sorted((entry1, entry2) -> -entry1.getValue().compareTo(entry2.getValue()))
+                    .map(Map.Entry::getKey)
+                    .collect(Collectors.toList());
+        }
+
+        // Произведение всех чисел потока по модулю 'modulo'
+        // (все числа от 0 до 10000)
+        public static int moduloProduction(IntStream stream, int modulo) {
+            return stream.reduce(1, (left, right) -> left * right % modulo);
+        }
+
+        // Вернуть строку, состояющую из конкатенаций переданного массива, и окруженную строками "<", ">"
+        // см. тесты
+        public static String joinTo(String... strings) {
+            return "<" + Arrays.stream(strings).collect(Collectors.joining(", ")) + ">";
+        }
+
+        // Вернуть поток из объектов класса 'clazz'
+        public static <R> Stream<R> filterIsInstance(Stream<?> s, Class<R> clazz) {
+            return s.filter(clazz::isInstance).map(clazz::cast);
+        }
     }
-
-    // Список названий альбомов, отсортированный лексикографически по названию
-    public static List<String> allNamesSorted(Stream<Album> albums) {
-        throw new UnsupportedOperationException();
-    }
-
-    // Список треков, отсортированный лексикографически по названию, включающий все треки альбомов из 'albums'
-    public static List<String> allTracksSorted(Stream<Album> albums) {
-        throw new UnsupportedOperationException();
-    }
-
-    // Список альбомов, в которых есть хотя бы один трек с рейтингом более 95, отсортированный по названию
-    public static List<Album> sortedFavorites(Stream<Album> s) {
-        throw new UnsupportedOperationException();
-    }
-
-    // Сгруппировать альбомы по артистам
-    public static Map<Artist, List<Album>> groupByArtist(Stream<Album> albums) {
-        throw new UnsupportedOperationException();
-    }
-
-    // Сгруппировать альбомы по артистам (в качестве значения вместо объекта 'Artist' использовать его имя)
-    public static Map<Artist, List<String>> groupByArtistMapName(Stream<Album> albums) {
-        throw new UnsupportedOperationException();
-    }
-
-    // Число повторяющихся альбомов в потоке
-    public static long countAlbumDuplicates(Stream<Album> albums) {
-        throw new UnsupportedOperationException();
-    }
-
-    // Альбом в котором максимум рейтинга минимален
-    // (если в альбоме нет ни одного трека, считать, что максимум рейтинга в нем --- 0)
-    public static Optional<Album> minMaxRating(Stream<Album> albums) {
-        throw new UnsupportedOperationException();
-    }
-
-    // Список альбомов, отсортированный по убыванию среднего рейтинга его треков (0, если треков нет)
-    public static List<Album> sortByAverageRating(Stream<Album> albums) {
-        throw new UnsupportedOperationException();
-    }
-
-    // Произведение всех чисел потока по модулю 'modulo'
-    // (все числа от 0 до 10000)
-    public static int moduloProduction(IntStream stream, int modulo) {
-        throw new UnsupportedOperationException();
-    }
-
-    // Вернуть строку, состояющую из конкатенаций переданного массива, и окруженную строками "<", ">"
-    // см. тесты
-    public static String joinTo(String... strings) {
-        throw new UnsupportedOperationException();
-    }
-
-    // Вернуть поток из объектов класса 'clazz'
-    public static <R> Stream<R> filterIsInstance(Stream<?> s, Class<R> clazz) {
-        throw new UnsupportedOperationException();
-    }
-}
--- src/test/java/ru/spbau/mit/SecondPartTasksTest.java
+++ src/test/java/ru/spbau/mit/SecondPartTasksTest.java
@@ -1,28 +1,117 @@
 package ru.spbau.mit;
 
+import com.google.common.collect.ImmutableMap;
 import org.junit.Test;
 
-import static org.junit.Assert.fail;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static junit.framework.Assert.fail;
+import static junitx.framework.Assert.assertEquals;
+import static junitx.framework.Assert.assertTrue;
+import static ru.spbau.mit.SecondPartTasks.*;
 
 public class SecondPartTasksTest {
 
     @Test
     public void testFindQuotes() {
-        fail();
+        try {
+            File file0 = File.createTempFile(FILE_NAME_0, FILE_EXTENSION);
+            Files.write(file0.toPath(),
+                    Arrays.asList(LINE_0, LINE_1, LINE_2));
+
+            File file1 = File.createTempFile(FILE_NAME_1, FILE_EXTENSION);
+            Files.write(file1.toPath(),
+                    Arrays.asList(LINE_2, LINE_3, LINE_4, LINE_5));
+
+            assertEquals(Arrays.asList(LINE_0, LINE_2, LINE_2, LINE_3),
+                    findQuotes(
+                            Arrays.asList(file0.getPath(), file1.getPath()),
+                            QUOTE_0));
+
+            assertEquals(Arrays.asList(LINE_1, LINE_2, LINE_2, LINE_4),
+                    findQuotes(
+                            Arrays.asList(file0.getPath(), file1.getPath()),
+                            QUOTE_1));
+        } catch (IOException e) {
+            e.printStackTrace();
+            fail("File creation failed");
+        }
     }
 
     @Test
     public void testPiDividedBy4() {
-        fail();
+        final double eps = 1e-3;
+        System.out.println(piDividedBy4());
+        assertTrue(Math.abs(piDividedBy4() - Math.PI / 4) < eps);
     }
 
     @Test
     public void testFindPrinter() {
-        fail();
+        assertEquals(AUTHOR_2,
+                findPrinter(ImmutableMap.of(
+                        AUTHOR_0, COMPOSITIONS_0,
+                        AUTHOR_2, COMPOSITIONS_2,
+                        AUTHOR_1, COMPOSITIONS_1)));
+        assertEquals(AUTHOR_1,
+                findPrinter(ImmutableMap.of(
+                        AUTHOR_0, COMPOSITIONS_0,
+                        AUTHOR_1, COMPOSITIONS_1)));
     }
 
     @Test
     public void testCalculateGlobalOrder() {
-        fail();
+        assertEquals(ImmutableMap.of(
+                        PRODUCT_0, 1,
+                        PRODUCT_1, 7,
+                        PRODUCT_2, 8
+                ),
+                calculateGlobalOrder(
+                        Arrays.asList(
+                                ImmutableMap.of(
+                                        PRODUCT_0, 1,
+                                        PRODUCT_1, 2),
+                                ImmutableMap.of(
+                                        PRODUCT_1, 2,
+                                        PRODUCT_2, 3
+                                ),
+                                ImmutableMap.of(
+                                        PRODUCT_1, 3,
+                                        PRODUCT_2, 5
+                                ))));
     }
+
+    // CHECKSTYLE:OFF
+
+    // testFindQuotes
+    private static final String FILE_NAME_0 = "temp0";
+    private static final String FILE_NAME_1 = "temp1";
+    private static final String FILE_EXTENSION = ".txt";
+    private static final String LINE_0 = "Hello world!";
+    private static final String LINE_1 = "Some test line";
+    private static final String LINE_2 = "Test line with quote 'world'";
+    private static final String LINE_3 = "world in fire!";
+    private static final String LINE_4 = "Thetextlinearization";
+    private static final String LINE_5 = "Plain text with no quotes";
+    private static final String QUOTE_0 = "world";
+    private static final String QUOTE_1 = "line";
+
+    // testFindPrinter
+    private static final String AUTHOR_0 = "AUTHOR_0";
+    private static final String AUTHOR_1 = "AUTHOR_1";
+    private static final String AUTHOR_2 = "AUTHOR_2";
+    private static final List<String> COMPOSITIONS_0 = Collections.emptyList();
+    private static final List<String> COMPOSITIONS_1 = Arrays.asList("Text1", "LongText1", "NotShortText1");
+    private static final List<String> COMPOSITIONS_2 = Arrays.asList("Text2", "LongText2", "VeryVeryLongText2");
+
+    // testCalculateGlobalOrder
+    private static final String PRODUCT_0 = "PRODUCT_0";
+    private static final String PRODUCT_1 = "PRODUCT_1";
+    private static final String PRODUCT_2 = "PRODUCT_2";
+
+    // CHECKSTYLE:ON
 }
