76c3c26c8aab0ad100bd06b79e51fbe015ede02a
Semionn <mrSemionn@gmail.com>
2016-04-19 15:05:34 +0300

    add cw2 (Injector)

--- src/main/java/ru/spbau/mit/Injector.java
+++ src/main/java/ru/spbau/mit/Injector.java
@@ -1,9 +1,10 @@
 package ru.spbau.mit;
 
-import java.util.List;
-
+import java.lang.reflect.Constructor;
+import java.util.*;
 
 public final class Injector {
+
     private Injector() {
     }
 
@@ -12,6 +13,63 @@ public final class Injector {
      * `implementationClassNames` for concrete dependencies.
      */
     public static Object initialize(String rootClassName, List<String> implementationClassNames) throws Exception {
-        throw new IllegalStateException();
+        Map<String, Object> createdClasses = new HashMap<>();
+        Set<String> resolvingClasses = new HashSet<>();
+        return resolve(rootClassName, implementationClassNames, createdClasses, resolvingClasses);
+    }
+
+    private static Object resolve(String className, List<String> implementationClassNames,
+                                  Map<String, Object> createdClasses, Set<String> resolvingClasses)
+            throws Exception {
+        if (resolvingClasses.contains(className)) {
+            throw new InjectionCycleException();
+        }
+
+        Object result = null;
+        if (createdClasses.containsKey(className)) {
+            result = createdClasses.get(className);
+        }
+
+        if (result != null) {
+            return result;
+        }
+
+        resolvingClasses.add(className);
+
+        Constructor<?> constructor = Class.forName(className).getConstructors()[0];
+        List<Object> constructorArgs = new ArrayList<>();
+        for (String depImpl : getConstructorDependencies(constructor, implementationClassNames)) {
+            constructorArgs.add(resolve(depImpl, implementationClassNames, createdClasses, resolvingClasses));
+        }
+        result = constructor.newInstance(constructorArgs.toArray());
+        resolvingClasses.remove(className);
+        createdClasses.put(className, result);
+
+        return result;
+    }
+
+    private static List<String> getConstructorDependencies(Constructor<?> constructor,
+                                                           List<String> implementationClassNames)
+            throws ClassNotFoundException, AmbiguousImplementationException, ImplementationNotFoundException {
+        Class<?>[] parameters = constructor.getParameterTypes();
+        List<String> dependencyClasses = new ArrayList<>();
+        for (Class<?> depClass : parameters) {
+            String depClassName = null;
+            for (String implementedClassName : implementationClassNames) {
+                Class implementedClass = Class.forName(implementedClassName);
+                if (depClass.isAssignableFrom(implementedClass)) {
+                    if (depClassName == null) {
+                        depClassName = implementedClassName;
+                    } else {
+                        throw new AmbiguousImplementationException();
+                    }
+                }
+            }
+            if (depClassName == null) {
+                throw new ImplementationNotFoundException();
+            }
+            dependencyClasses.add(depClassName);
+        }
+        return dependencyClasses;
     }
 }
